47bd5d2378574b7bfef6b7d62b4c8a4c92e7794f
rmlance <reidlance.rml@gmail.com>
2020-05-14 17:19:48 -0400

    Updated styling, added logic to handle new users

---
+++ app/javascript/react/components/Instructions.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Instructions = props => {
+
+  return(
+    <div className="instruction-container">
+      <div className="dominate-block">
+      </div>
+        <h1 className="dominate">Dominate the market.</h1>
+        <p className="text">Start building your portfolio now.</p>
+    </div>
+  )
+}
+
+export default Instructions;
